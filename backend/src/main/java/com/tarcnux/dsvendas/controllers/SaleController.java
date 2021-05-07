bc428a75c059aaad038b44365b46bcad308e6d76
Tarcisio Nunes <github@tarcnux.com.br>
2021-05-06 23:06:40 -0300

    Group by search

--- backend/src/main/java/com/tarcnux/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/tarcnux/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.tarcnux.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tarcnux.dsvendas.dto.SaleDTO;
+import com.tarcnux.dsvendas.dto.SaleSuccessDTO;
+import com.tarcnux.dsvendas.dto.SaleSumDTO;
 import com.tarcnux.dsvendas.services.SaleService;
 
 @RestController
@@ -22,5 +26,17 @@ public class SaleController {
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
 		Page<SaleDTO> page = service.findAll(pageable);
 		return ResponseEntity.ok(page);
+	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
+		return ResponseEntity.ok(list);
 	}
 }
--- backend/src/main/java/com/tarcnux/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/tarcnux/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.tarcnux.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tarcnux.dsvendas.dto.SaleDTO;
+import com.tarcnux.dsvendas.dto.SaleSuccessDTO;
+import com.tarcnux.dsvendas.dto.SaleSumDTO;
 import com.tarcnux.dsvendas.entities.Sale;
 import com.tarcnux.dsvendas.repositories.SaleRepository;
 import com.tarcnux.dsvendas.repositories.SellerRepository;
@@ -26,5 +30,16 @@ public class SaleService {
 		Page<Sale> result = saleRepository.findAll(pageable);		
 		return result.map(x -> new SaleDTO(x));
 	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return saleRepository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return saleRepository.successGroupedBySeller();
+	}
+	
 
 }
